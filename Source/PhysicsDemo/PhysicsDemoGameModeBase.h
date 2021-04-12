cb3589b96c4c5502396600f3ad8a91ec05283e69
Jasfiq Rahman <jasfiqrahman15@gmail.com>
2021-04-12 23:29:12 +0530

    Initial commit

---
+++ Source/PhysicsDemo/PhysicsDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhysicsDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PHYSICSDEMO_API APhysicsDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
